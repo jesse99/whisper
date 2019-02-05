1fc8f06b13ecd9fd4d804422dd081490f9064659
Jesse Jones <jesse9jones@gmail.com>
2019-02-04 21:08:26 -0800

    Re-adding files with executable bit cleared

---
+++ Whisper/Source/BackEnd/Runtime/Source/Debugging/Mac/SLFunctions.c
@@ -0,0 +1,1 @@
+#include <SLFunctions.h>void SLDisable(void)			// Spotlight will replace these functions when it starts up{}void SLEnable(void){}void SLEnterInterrupt(void){}void SLLeaveInterrupt(void){}void SLResetLeaks(void){}
